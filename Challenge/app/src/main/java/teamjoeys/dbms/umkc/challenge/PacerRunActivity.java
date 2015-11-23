42a2edda3d0caded612d1c1b0ab78f6944ca88c5
Daniel Galarza <dvgalarza@gmail.com>
2015-11-23 17:47:18 -0600

    Basic PacerRun activity template

---
+++ Challenge/app/src/main/java/teamjoeys/dbms/umkc/challenge/PacerRunActivity.java
@@ -0,0 +1,14 @@
+package teamjoeys.dbms.umkc.challenge;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class PacerRunActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pacer_run);
+    }
+
+}
